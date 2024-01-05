c7ffec114fd80b31e0fef48eb652b3de080c3982
pranjalsharmahp <96167870+pranjalsharmahp@users.noreply.github.com>
2024-01-05 08:37:12 +0530

    Optimised the code
    Optimised the readability and working of the code by adding functions and removing the duplicate code;

--- Assets/ChessTemplate/Assets/Chess/Scripts/Core/ChessPieceSelectionHandler.cs
+++ Assets/ChessTemplate/Assets/Chess/Scripts/Core/ChessPieceSelectionHandler.cs
@@ -7,6 +7,7 @@ public class ChessPieceSelectionHandler : MonoBehaviour
     Ray ray;
     public Camera cameraa;
     public ChessPlayerPlacementHandler chessPlayerPlacementHandler;
+    private const int chessBoardSize=8;
 
     // Update is called once per frame
     void Update()
@@ -34,183 +35,98 @@ public class ChessPieceSelectionHandler : MonoBehaviour
         chessPlayerPlacementHandler=SelectedPiece.gameObject.GetComponent<ChessPlayerPlacementHandler>();
         switch(SelectedPiece.gameObject.tag){
             case "Black Pawn":
-                if(chessPlayerPlacementHandler.row==1 && ChessBoardPlacementHandler.Instance._occupiedTiles[2,chessPlayerPlacementHandler.column]!="B"){
-                    ChessBoardPlacementHandler.Instance.Highlight(2,chessPlayerPlacementHandler.column);
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[3,chessPlayerPlacementHandler.column]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(3,chessPlayerPlacementHandler.column);
-                    }
-                    
-                }
-                else{
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[chessPlayerPlacementHandler.row+1,chessPlayerPlacementHandler.column]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(chessPlayerPlacementHandler.row+1,chessPlayerPlacementHandler.column);
-                    }
-                }
-                Debug.Log("Black Pawn Selected");
+                HighlightBlackPawnMoves();
                 break;
-            
             case "Black Rook":
-                for(int i=chessPlayerPlacementHandler.row+1;i<8;i++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,chessPlayerPlacementHandler.column]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,chessPlayerPlacementHandler.column);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row-1;i>=0;i--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,chessPlayerPlacementHandler.column]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,chessPlayerPlacementHandler.column);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int j=chessPlayerPlacementHandler.column+1;j<8;j++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[chessPlayerPlacementHandler.row,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(chessPlayerPlacementHandler.row,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int j=chessPlayerPlacementHandler.column-1;j>=0;j--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[chessPlayerPlacementHandler.row,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(chessPlayerPlacementHandler.row,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
+                HighlightRookMoves();
                 break;
-
             case "Black Bishop":
-                for(int i=chessPlayerPlacementHandler.row+1,j=chessPlayerPlacementHandler.column+1;i<8&&j<8;i++,j++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row-1,j=chessPlayerPlacementHandler.column-1;i>=0&&j>=0;i--,j--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row-1,j=chessPlayerPlacementHandler.column+1;i>=0&&j<8;i--,j++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row+1,j=chessPlayerPlacementHandler.column-1;i<8&&j>=0;i++,j--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
+                HighlightBishopMoves();
                 break;
             case "Black Knight":
-                int currentRow=chessPlayerPlacementHandler.row;
-                int currentColumn=chessPlayerPlacementHandler.column;
-                int[,] knightMoves={{2,1},{2,-1},{1,2},{1,-2},{-2,1},{-2,-1},{-1,2},{-1,-2}};
-                for(int i=0;i<knightMoves.GetLength(0);i++){
-                    int newRow=currentRow+knightMoves[i,0];
-                    int newColumn=currentColumn+knightMoves[i,1];
-                    if(newRow>=0 && newRow<8 && newColumn>=0 && newColumn<8 && ChessBoardPlacementHandler.Instance._occupiedTiles[newRow,newColumn]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(newRow,newColumn);
-                    }
-                }
+                HighlightKnightMoves();
                 break;
             case "Black Queen":
-                for(int i=chessPlayerPlacementHandler.row+1;i<8;i++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,chessPlayerPlacementHandler.column]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,chessPlayerPlacementHandler.column);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row-1;i>=0;i--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,chessPlayerPlacementHandler.column]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,chessPlayerPlacementHandler.column);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int j=chessPlayerPlacementHandler.column+1;j<8;j++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[chessPlayerPlacementHandler.row,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(chessPlayerPlacementHandler.row,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int j=chessPlayerPlacementHandler.column-1;j>=0;j--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[chessPlayerPlacementHandler.row,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(chessPlayerPlacementHandler.row,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row+1,j=chessPlayerPlacementHandler.column+1;i<8&&j<8;i++,j++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row-1,j=chessPlayerPlacementHandler.column-1;i>=0&&j>=0;i--,j--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row-1,j=chessPlayerPlacementHandler.column+1;i>=0&&j<8;i--,j++){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
-                for(int i=chessPlayerPlacementHandler.row+1,j=chessPlayerPlacementHandler.column-1;i<8&&j>=0;i++,j--){
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[i,j]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(i,j);
-                    }
-                    else{
-                        break;
-                    }
-                }
+                HighlightRookMoves();
+                HighlightBishopMoves();
                 break;
             case "Black King":
-                currentRow=chessPlayerPlacementHandler.row;
-                currentColumn=chessPlayerPlacementHandler.column;
-                int[,] kingMoves={{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-                for(int i=0;i<kingMoves.GetLength(0);i++){
-                    int newRow=currentRow+kingMoves[i,0];
-                    int newColumn=currentColumn+kingMoves[i,1];
-                    if(ChessBoardPlacementHandler.Instance._occupiedTiles[newRow,newColumn]!="B"){
-                        ChessBoardPlacementHandler.Instance.Highlight(newRow,newColumn);
-                    }
-                }
+                HighlightKingMoves();
                 break;
-            }
-
-                
         }
+
+    }
+    void HighlightBlackPawnMoves(){
+        int currentRow=chessPlayerPlacementHandler.row;
+        int currentColumn=chessPlayerPlacementHandler.column;
+        if(currentRow==1 && ChessBoardPlacementHandler.Instance._occupiedTiles[2,currentColumn]!="B"){
+            ChessBoardPlacementHandler.Instance.Highlight(2,currentColumn);
+            if(ChessBoardPlacementHandler.Instance._occupiedTiles[3,currentColumn]!="B"){
+                ChessBoardPlacementHandler.Instance.Highlight(3,currentColumn);
+            }
+        }
+        else{
+            if(ChessBoardPlacementHandler.Instance._occupiedTiles[currentRow+1,currentColumn]!="B"){
+                ChessBoardPlacementHandler.Instance.Highlight(currentRow+1,currentColumn);
+            }
+        }
+    }
+
+    void HighlightRookMoves(){
+        HighlightDirectionalMoves(0,1);
+        HighlightDirectionalMoves(0,-1);
+        HighlightDirectionalMoves(1,0);
+        HighlightDirectionalMoves(-1,0);
+    }
+
+    void HighlightBishopMoves(){
+        HighlightDirectionalMoves(1,1);
+        HighlightDirectionalMoves(-1,1);
+        HighlightDirectionalMoves(-1,-1);
+        HighlightDirectionalMoves(1,-1);
+    }
+
+    void HighlightKingMoves(){
+            int currentRow=chessPlayerPlacementHandler.row;
+            int currentColumn=chessPlayerPlacementHandler.column;
+            int[,] kingMoves={{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+            for(int i=0;i<kingMoves.GetLength(0);i++){
+                int newRow=currentRow+kingMoves[i,0];
+                int newColumn=currentColumn+kingMoves[i,1];
+                if(ChessBoardPlacementHandler.Instance._occupiedTiles[newRow,newColumn]!="B"){
+                    ChessBoardPlacementHandler.Instance.Highlight(newRow,newColumn);
+                }
+            }
+    }
+
+    void HighlightKnightMoves(){
+        int currentRow=chessPlayerPlacementHandler.row;
+            int currentColumn=chessPlayerPlacementHandler.column;
+            int[,] knightMoves={{2,1},{2,-1},{1,2},{1,-2},{-2,1},{-2,-1},{-1,2},{-1,-2}};
+            for(int i=0;i<knightMoves.GetLength(0);i++){
+                int newRow=currentRow+knightMoves[i,0];
+                int newColumn=currentColumn+knightMoves[i,1];
+                if(newRow>=0 && newRow<8 && newColumn>=0 && newColumn<8 && ChessBoardPlacementHandler.Instance._occupiedTiles[newRow,newColumn]!="B"){
+                    ChessBoardPlacementHandler.Instance.Highlight(newRow,newColumn);
+                }
+            }
+    }
+
+    void HighlightDirectionalMoves(int rowIncrement,int columnIncrement){
+        int currentRow=chessPlayerPlacementHandler.row;
+        int currentColumn=chessPlayerPlacementHandler.column;
+        for(int i=currentRow+rowIncrement,j=currentColumn+columnIncrement; (i>=0 && i<chessBoardSize && j>=0 && j< chessBoardSize); i+=rowIncrement,j+=columnIncrement){
+            if(!TryHighlightPosition(i,j)) break;
+        }
+    }
+
+    bool TryHighlightPosition(int row,int column){
+        if(ChessBoardPlacementHandler.Instance._occupiedTiles[row,column]!="B"){
+            ChessBoardPlacementHandler.Instance.Highlight(row,column);
+            return true;
+        }
+        return false;
+    }
+
 }
 
